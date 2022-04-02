bb92d49ae742ab153c84e0181c113bf01848b99d
exordium-vic3 <eazyspeeder@gmail.com>
2022-04-02 08:45:25 -0400

    uploaded overlay

---
+++ place/overlay.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         FUCK NFTS Overlay
+// @namespace    http://tampermonkey.net/
+// @version      2.2
+// @description  Keep the canvas beautiful!
+// @author       oralekin from osu! /r/osuplace
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://exordium.breadcraft.me/place/overlay.js
+// @downloadURL  https://exordium.breadcraft.me/place/overlay.js
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://exordium.breadcraft.me/place/potential%20overlay.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
