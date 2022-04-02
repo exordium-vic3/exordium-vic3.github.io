33654da888acd3c4c479b21abcc6b3c2f0ec805a
exordium-vic3 <eazyspeeder@gmail.com>
2022-04-02 16:00:51 -0400

    fixed script

--- place/new overlay.user.js
+++ place/new overlay.user.js
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://exordium.breadcraft.me/place/new%20overlay.png";
+            i.src = "https://exordium.breadcraft.me/place/NEW%20OVERLAY.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated; image-rendering: -moz-crisp-edges;width: 2000pxheight: 1000px;";
             console.log(i);
             return i;
